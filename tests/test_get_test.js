f8e1bac020b7cd9fa6f201289c16fc45e0ece4a1
Steven R. Loomis <srl295@gmail.com>
2026-08-11 11:12:12 -0500

    chore: add test that CLI works

---
+++ tests/test_get_test.js
@@ -0,0 +1,15 @@
+/**
+ * Copyright (c) 2022 Code Hive Tx, LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+const t = require('tap');
+var expect = require('chai').expect;
+const Cli = require('../lib/cli');
+
+
+t.test('./bin/goosebay -K test --get', async t => {
+	const argv = { _: [], K: 'test', configName: 'test', get: true, P: true };
+	const cli = new Cli();
+	await cli.run(argv);
+});
