171977f096184649adfeb1f8525b399ee80d3fac
Steven R. Loomis <srl295@gmail.com>
2026-08-15 17:46:54 -0500

    chore: lint

--- tests/test_get_test.js
+++ tests/test_get_test.js
@@ -4,12 +4,11 @@
  */
 
 const t = require('tap');
-var expect = require('chai').expect;
+//var expect = require('chai').expect;
 const Cli = require('../lib/cli');
 
-
-t.test('./bin/goosebay -K test --get', async t => {
-	const argv = { _: [], K: 'test', configName: 'test', get: true, P: true };
-	const cli = new Cli();
-	await cli.run(argv);
+t.test('./bin/goosebay -K test --get', async (/*t*/) => {
+    const argv = { _: [], K: 'test', configName: 'test', get: true, P: true };
+    const cli = new Cli();
+    await cli.run(argv);
 });
